8d4804922893f62ee7a3602ca8a0c0580c22ec43
IvanKovalov <kovalevvana8@gmail.com>
2022-02-10 20:26:35 +0300

    Add new main solution
     In this commit added function, which will be recieve coefficient of
     quadratic equation and then will solve equation. Of course added a
     simple outut in console basic equation and roots.

--- equationMaster.js
+++ equationMaster.js
@@ -1,2 +1,23 @@
 'use stict'
 
+const solution = (coef) => {
+    let x1, x2;
+    const a = coef[0];
+    const b = coef[1];
+    const c = coef[2];
+    console.log('Equation is:' + a + 'x^2 + ' + b + 'x + ' + c + ' = 0\n');
+    const D = (b*b  - 4*a*c);
+    if (D > 0){
+        x1 = (-1*b + Math.sqrt(D))/(2*a);
+        x2 = (-1*b - Math.sqrt(D))/(2*a);
+        console.log('There are 2 roots\nx1 = ' + x1 + '\nx2 = '+ x2);
+    }
+    else if (D === 0){
+        x1 = (-1*b)/(2*a);
+        x2 = x1; 
+        console.log('There are 1 root\n x1 = ' + x1);
+    }
+    else{
+        console.log('There are 0 roots\n');
+    }
+}
